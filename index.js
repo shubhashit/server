c5ffc3648f2855fb1536e32b5d13d0ca10503de2
Shubhashit <shubhashitjain@gmail.com>
2023-12-27 22:58:03 +0530

    adding http server

--- index.js
+++ index.js
@@ -2,18 +2,26 @@ const express = require('express');
 const http = require('http');
 const socket = require('socket.io');
 
-const app = express();
+const app = http.createServer((req , res)=>{
+    if (req.url === '/api' && req.method === 'GET') {
+        res.end(
+            JSON.stringify({
+                messege : "this is api"
+            })
+        )
+    }  else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(
+            JSON.stringify({
+                message: 'Route Not Found: Please use the api/products endpoint',
+            })
+        );
+    }
 
-app.get('/' , (req , res)=>{
-    res.send('hellow world')
-})
 
-app.get('/api' , (req , res)=>{
-    res.json({
-        messege : 'this is a test api route',
-        status : "400"
-    })
-})
-app.listen(3000,'0.0.0.0' , ()=>{
+});
+
+
+app.listen(3000  , ()=>{
     console.log('listen on port')
-})
+})  
