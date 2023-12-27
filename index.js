e8010e8211465d66660fcec061d03eb4af2a1ed3
Shubhashit <shubhashitjain@gmail.com>
2023-12-27 21:49:54 +0530

    initialization of server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const http = require('http');
+const socket = require('socket.io');
+
+const app = express();
+
+app.get('/' , (req , res)=>{
+    res.send('hellow world')
+})
+
+app.get('/api' , (req , res)=>{
+    res.json({
+        messege : 'this is a test api route',
+        status : "400"
+    })
+})
+app.listen(3000 , ()=>{
+    console.log('listen on port')
+})
