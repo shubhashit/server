f2b97d7f4b8684f955c8616c76d3a56145ba7c76
Shubhashit <shubhashitjain@gmail.com>
2023-12-28 15:00:28 +0530

    express app without http

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-// Importing the http module
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-// Creating a server that listens on port 3000
-const server = http.createServer((req, res) => {
-    // Set the response header
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+app.get('/api' , (req , res) =>{
+    res.json({
+        messsage : "this is the api call",
+    })
+})
 
-    // Send the response body
-    res.end('Hello, World!\n');
-});
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
-// Start the server on port 3000
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}/`);
-});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+
+// http code structure 
+// // Importing the http module
+// const http = require('http');
+
+// // Creating a server that listens on port 3000
+// const server = http.createServer((req, res) => {
+//     // Set the response header
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+//     // Send the response body
+//     res.end('Hello, World!\n');
+// });
+
+// // Start the server on port 3000
+// const port = process.env.PORT || 3000;
+// server.listen(port, () => {
+//     console.log(`Server is running at http://localhost:${port}/`);
+// });
